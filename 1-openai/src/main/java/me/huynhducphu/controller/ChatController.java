69ed0ecc8d1f9f2aae5ef576761dac81ef007347
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-05-02 14:55:32 +0700

    ignore env

--- 1-openai/src/main/java/me/huynhducphu/controller/ChatController.java
+++ 1-openai/src/main/java/me/huynhducphu/controller/ChatController.java
@@ -1,7 +1,34 @@
 package me.huynhducphu.controller;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- *  Admin 5/2/2026
- *  
-**/public class ChatController {
+ * Admin 5/2/2026
+ *
+ **/
+@RestController
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class ChatController {
+
+    ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder
+
+                .build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message) {
+        return chatClient
+                .prompt(message)
+                .call()
+                .content();
+    }
+
 }
